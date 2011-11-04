3167e52cea16423dc2b21ad38278a5122cb28eb6
Fwirt <sploopby@gmail.com>
2011-11-04 11:14:32 -0700

    Merged descriptions with mains
    Merged all of the "desc.txt" files with the "Main.java" files as comments
    at the top to facilitate printing

--- 2011/coj/bamboo/Main.java
+++ 2011/coj/bamboo/Main.java
@@ -1,3 +1,30 @@
+/***
+description
+
+Bamboo is a species of plant that grows vertically from the ground. Despite its incredible strength, and which reaches several meters in height, the strong northerly wind eventually overcomes its trunk. A group of young scientists study some properties of this plant, and will need to develop a software to determine the average distance from ground which breaks the bamboo stem.
+
+input specification
+
+The first line contains the number of cases to process (1 <= T <= 50). Each case consists of a line containing a number H (0 < H <= 50) which represents the initial height of the plant in question before breaking, and a number D, the distance between the base of the trunk of the plant and the point where it is supported the top of the plant after break and fall down.
+
+output specification
+
+For every plant should be printed on one line, the value of the height at which it breaks. In the T+1 line, should be printed the average distance from ground which breaks the bamboo stem of the processed plants. All the calculated values to be written with one decimal place of accuracy.
+
+sample input
+
+3
+50.0 20.5
+18.0 6.0
+32.0 10.2
+sample output
+
+20.8
+8.0
+14.4
+14.4
+***/
+
 import java.util.*;
 
 public class Main {
--- 2011/coj/genelec/Main.java
+++ 2011/coj/genelec/Main.java
@@ -1,3 +1,36 @@
+/***
+description
+
+General Election is over, now it is time to count the votes! There are n (2 <= n <= 5) candidates and m (1 <= m <= 100) vote regions. Given the number of votes for each candidate from each region, determine which candidate is the winner (one with the most votes).
+
+input specification
+
+The first line of input contains an integer T, the number of test cases follow. Each test case starts with an integer n and m denoting the number of candidate and number of region. The next m lines each contains n integers, V1, V2, ..., Vn (0 <= Vi <= 1000) the number of votes for candidate i.
+
+output specification
+
+For each test case, output in a line the winning candidate. You may safely assume that the winner is unique.
+
+sample input
+
+2
+3 3
+159 213 450
+512 890 993
+215 420 397
+2 5
+40 64
+35 12
+102 58
+43 15
+79 41
+
+sample output
+
+3
+1
+***/
+
 import java.util.*;
 
 public class Main {
